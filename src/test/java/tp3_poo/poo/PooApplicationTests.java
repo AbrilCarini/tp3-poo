62d8b5d71cc2b99246a42b1698d85c74cc0a6b02
Abril <abrilcarini14@gmail.com>
2021-11-22 21:07:45 -0300

    commit inicial

---
+++ src/test/java/tp3_poo/poo/PooApplicationTests.java
@@ -0,0 +1,13 @@
+package tp3_poo.poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
